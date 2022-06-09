6e2333d45f45624e1bb770b3ed497c2b9ea83736
yong.teng <webmaster@buession.com>
2022-06-10 00:22:49 +0800

    调整logo大小

--- theme/ydoc-theme-demo/components/Logo.jsx
+++ theme/ydoc-theme-demo/components/Logo.jsx
@@ -1,5 +1,5 @@
 <a href={relePath(props.distPath, "index.html")} target="_self">
   {
-    props.nav.logo ? <img className="logo" width="36" src={relePath(props.distPath, props.nav.logo)} /> : null
+    props.nav.logo ? <img className="logo" width="70" src={relePath(props.distPath, props.nav.logo)} /> : null
   }
 </a>
